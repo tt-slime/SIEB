2244c62dd5289891b87862c87452d848b27c06ae
tt-slime <80716080+tt-slime@users.noreply.github.com>
2024-03-07 10:56:51 -0300

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,11 +1,12 @@
+
 function mostrarModal() {
-    document.getElementById('modal').style.display = 'block';
+  document.getElementById('modal').style.display = 'block';
 }
 
 function fecharModal(event) {
-    if (event.target === document.getElementById('modal') || event.target.classList.contains('close')) {
-        document.getElementById('modal').style.display = 'none';
-    }
+  if (event.target === document.getElementById('modal') || event.target.classList.contains('close')) {
+      document.getElementById('modal').style.display = 'none';
+  }
 }
 
 document.getElementById('login-link').addEventListener('click', mostrarModal);
@@ -15,22 +16,20 @@ document.getElementById('login-link').addEventListener('click', mostrarModal);
 
 
 document.addEventListener("DOMContentLoaded", function() {
-    let slideIndex = 0;
-    showSlides();
-  
-    function showSlides() {
-      let slides = document.getElementsByClassName("slide_inicial");
-      for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-      }
-      slideIndex++;
-      if (slideIndex > slides.length) {
-        slideIndex = 1;
-      }
-      slides[slideIndex - 1].style.display = "block";
-      setTimeout(showSlides, 2500); // Muda de slide a cada 2 segundos
-    }
-  });
+  let slideIndex = 0;
+  showSlides();
 
-  
-  
+  function showSlides() {
+    let slides = document.getElementsByClassName("carrossel_inicial_imagem");
+    for (let i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {
+      slideIndex = 1;
+    }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 2500); // Muda de slide a cada 2 segundos
+  }
+});
+
